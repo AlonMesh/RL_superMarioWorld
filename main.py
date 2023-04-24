ae931ea3035b3edcd75d2b52e67c0b43f109b771
AlonMesh <alon3400@gmail.com>
2023-04-24 23:20:46 +0300

    Edit rewards & Adding statistics and graphes

--- main.py
+++ main.py
@@ -7,9 +7,49 @@ from PIL import Image
 import cv2
 import neat
 import pickle
+import pyautogui
+import matplotlib.pyplot as plt
+
+
+class PlotReporter(neat.reporting.BaseReporter):
+    def __init__(self):
+        self.fitnesses = []
+        self.bests = []
+        self.worsts = []
+        self.avg = []
+
+    def post_evaluate(self, config, population, species, best_genome):
+        global all_fit
+        generation = len(self.fitnesses) + 1
+        fitnesses = [x.fitness for x in population.values()]
+        self.fitnesses.append(fitnesses)
+
+        best_fitness = max(fitnesses)
+        self.bests.append(best_fitness)
+
+        worst_fitness = min(fitnesses)
+        self.worsts.append(worst_fitness)
+
+        avg_fitness = sum(fitnesses) / len(fitnesses)
+        self.avg.append(avg_fitness)
+
+        plt.xticks(ticks=(range(0, generation)))
+
+        plt.plot(self.bests, label='Best')
+        plt.plot(self.worsts, label='Worst')
+        plt.plot(self.avg, label='Average')
+        plt.title('Fitnesses over generations')
+        plt.xlabel('Generation')
+        plt.ylabel('Fitness')
+        plt.legend()
+        plt.show()
+
+        # Reset all_fit for each new generation
+        all_fit = []
 
 
 def eval_genomes(genomes, config):
+    global all_fit
     img_array = []
     for genome_id, genome in genomes:
         ob = env.reset()
@@ -30,12 +70,33 @@ def eval_genomes(genomes, config):
         xpos = 0
         max_xpos = 0
 
+        coins = 0
+        powerups = 0
+        yoshiCoins = 0
+
         done = False
+        cv2.namedWindow("main", cv2.WINDOW_NORMAL)
 
         while not done:
-            env.render()
-
+            # env.render()
             frame += 1
+
+            scaled_img = cv2.cvtColor(ob, cv2.COLOR_BGR2RGB)
+
+            computer_img = cv2.resize(scaled_img, (iny, inx))
+            computer_img = cv2.cvtColor(computer_img, cv2.COLOR_BGR2GRAY)
+
+            scaled_img_copy = scaled_img.copy()
+
+            x_offset = y_offset = 10
+            computer_img_resized = computer_img
+
+            scaled_img_copy[y_offset:y_offset + computer_img_resized.shape[0],
+            x_offset:x_offset + computer_img_resized.shape[1], :] = cv2.cvtColor(computer_img_resized,
+                                                                                 cv2.COLOR_GRAY2RGB)
+
+            cv2.imshow('main', scaled_img_copy)
+            cv2.waitKey(1)
 
             ob = cv2.resize(ob, (inx, iny))
             ob = cv2.cvtColor(ob, cv2.COLOR_BGR2GRAY)
@@ -56,8 +117,16 @@ def eval_genomes(genomes, config):
             xpos = info['x']
 
             if xpos > max_xpos:
-                fitness_current += 1  # itll change to rew
+                fitness_current += 5  # itll change to rew
                 max_xpos = xpos
+
+            if powerups > info['powerups']:
+                fitness_current += 200
+                fitness_current = info['powerups']
+
+            if yoshiCoins > info['yoshiCoins']:
+                fitness_current += 200
+                yoshiCoins = info['yoshiCoins']
 
             if fitness_current > current_max_fitness:
                 current_max_fitness = fitness_current
@@ -65,16 +134,25 @@ def eval_genomes(genomes, config):
             else:
                 counter += 1
 
+            fitness_current -= 0.4  # each frame lose
+
             # if the player doesn't move right in 250 frames, throw
             if done or counter == 250:
                 done = True
-                print(genome_id, fitness_current)
+                print(genome_id, round(fitness_current))
 
+            all_fit.append(fitness_current)
             genome.fitness = fitness_current
 
 
 # [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 #  A  B  X  Y  L  R  ↑  ↓  ←  →  Select Start
+
+# best_fitnesses = []
+# avg_fitnesses = []
+# worst_fitnesses = []
+# log_fitness = []
+all_fit = []
 
 # game_path = retro.data.get_romfile_path(game='SuperMarioWorld-Snes')
 game_path = r'C:\Users\Alon\PycharmProjects\NeuMario\venv\Lib\site-packages\retro\data\stable\SuperMarioWorld-Snes'
@@ -86,4 +164,19 @@ config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction, neat.DefaultS
 
 p = neat.Population(config)
 
+p.add_reporter(neat.StdOutReporter(True))
+stats = neat.StatisticsReporter()  # calls post_evaluate
+p.add_reporter(stats)
+p.add_reporter(neat.Checkpointer(10))  # every 10 generation - save
+plot_reporter = PlotReporter()
+p.add_reporter(plot_reporter)
+
 winner = p.run(eval_genomes)
+
+with open("winner.pkl", "wb") as output:
+    pickle.dump(winner, output, 1)
+
+with open("winner.pkl", "rb") as input_file:
+    loaded_winner = pickle.load(input_file)
+
+print("Loaded winner's fitness:", loaded_winner.fitness)
